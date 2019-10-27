58c3747b2120690d0bded252bd7bce1715c31c43
arianab <857@holbertonschool.com>
2019-10-27 23:23:22 +0000

    changed parameters

--- holberton.h
+++ holberton.h
@@ -5,20 +5,14 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-typedef struct newprint
-{
-	char * print;
-	int (*fp)(va_list, ...);
-} tprint;
-
 int _printf(const char *format, ...);
 
-int print_c(va_list, ...);
+int print_c(char c);
 
-int print_s(va_list, ...);
+int print_s(char *s);
 
-int print_i(va_list, ...);
+int print_i(int n);
 
-int print_d(va_list, ...);
+int print_d(int n);
 
 #endif /* HOLBERTON_H */
