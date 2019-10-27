7fa94404e65e3cce468f25b8cf6619a95ffdefc5
arianab <857@holbertonschool.com>
2019-10-27 03:02:04 +0000

    prints string

---
+++ printstring.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_s - function that prints string
+ * @s: string to be printed
+ * Return: void
+ */
+void print_s(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		print_c(s[i]);
+	}
+	print_c('\n');
+}
