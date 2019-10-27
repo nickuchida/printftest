c0f5eca4ef1377a684213d0dfe5fcca9f37fc6a1
arianab <857@holbertonschool.com>
2019-10-27 03:01:34 +0000

    prints character

---
+++ printchar.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * print_c - function that prints a character
+ * @c: character to be printed
+ * Return: character
+ */
+int print_c(char c)
+{
+	return (write(1, &c, 1));
+}
