696391b4b752a6d52e6a34f210760a8c72dc1953
ariana124 <ariana.bibiano@gmail.com>
2019-10-28 00:32:43 +0000

    fixed spacing & added comments

--- printdecimal.c
+++ printdecimal.c
@@ -1,17 +1,28 @@
 #include "holberton.h"
 
+/**
+ * print_d - prints a decimal (integer)
+ * @n: integer
+ * Return: decimal (integer)
+ */
 int print_d(int n)
 {
-        if (n < 0)
-        {
-                print_c('-');
-                n = -n;
-        }
-        if (n == 0)
-                print_c('0');
-        if (n / 10)
-                print_i(n / 10);
-        print_c(n % 10 + '0');
+	if (n < 0)
+	{
+		print_c('-');
+		n = -n;
+	}
+
+	if (n == 0)
+		print_c('0');
+
+	if (n / 10) /**
+		     * we use recursion to divide the number by 10 until we
+		     * reach the decimal point
+		     */
+		print_i(n / 10);
+
+	print_c(n % 10 + '0'); /* prints the last digit */
 
 	return (n);
 }
